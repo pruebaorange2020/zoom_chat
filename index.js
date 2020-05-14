53ced673a6075636803892a223b1b47397f653f6
pruebaorange2020 <pruebaorange2020@gmail.com>
2020-05-14 14:43:09 -0500

    actualizado package

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// require('dotenv').config()
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
